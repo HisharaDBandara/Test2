28718120bf9be497b00144ca248486f2e6771ce8
Hishara D bandara <hisharadeemantha14@gmail.com>
2025-02-24 11:12:17 +0530

    forloop

---
+++ loop/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace example
+{
+    class loop
+    {
+        static void Main(String[] args)
+        {
+
+            int x = 0;
+
+            while (x <= 4)
+            {
+                Console.WriteLine("Hello world");
+                x++;
+            }
+        }
+    }
+}
