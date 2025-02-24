c91ba0549e653950f21993b638125e459296ff02
Hishara D bandara <hisharadeemantha14@gmail.com>
2025-02-24 14:58:18 +0530

    atm

---
+++ Atm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace atm
+{
+    class Atm
+    {
+        static void Main(String[] args)
+        {
+            int atmPin = 1234;
+            int enterPin=0;
+            int attempt = 3;
+
+            
+
+            for (int i = 0; i < attempt; i++)
+            {
+                Console.Write("Enter the Pin : ");
+                enterPin = Convert.ToInt32(Console.ReadLine());
+
+                if (enterPin == atmPin)
+                {
+                    Console.WriteLine("Done");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Incorrect pin. {attempt - i - 1}attempt left: ");
+                }
+            }
+            Console.WriteLine("thank you");
+        }
+    }
+}
