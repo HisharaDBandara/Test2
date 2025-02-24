2650b3e78b7f131cee3fec4afa06b52aa5519e65
Hishara D bandara <hisharadeemantha14@gmail.com>
2025-02-24 11:12:28 +0530

    for Loop

---
+++ forloop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace forloop
+{
+    public class forloop
+    {
+       static void Main(String[] args)
+        {
+            int x, y, row;
+
+            Console.Write("Enter the Row: ");
+            row = Convert.ToInt32(Console.ReadLine());
+
+            for (x = 1; x <= row; x++)
+            {
+                for (y = 1; y <= x; y++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+
+            }
+
+            Console.ReadLine();
+        } 
+    }
+}
